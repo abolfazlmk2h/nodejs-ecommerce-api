a9ed2acb901923af45649de35c90d6f85821b6a9
abolfazlmk2h <mirzaei4273@gmail.com>
2024-03-26 22:52:35 +0330

    Add NotFound route handler

--- app/app.js
+++ app/app.js
@@ -3,13 +3,15 @@ dotenv.config()
 import express from 'express'
 import dbConnect from '../config/dbConnect.js'
 import userRoutes from '../routes/usersRoute.js'
-import globalErrHandler from '../middlewares/globalErrHandler.js'
+import { globalErrHandler, notFound } from '../middlewares/globalErrHandler.js'
 
 dbConnect()
 const app = express()
 
 app.use(express.json())
 app.use('/api/v1/users', userRoutes)
+
+app.use(notFound)
 app.use(globalErrHandler)
 
 export default app
--- middlewares/globalErrHandler.js
+++ middlewares/globalErrHandler.js
@@ -1,7 +1,14 @@
-export default function globalErrHandler(err, req, res, next) {
+export function globalErrHandler(err, req, res, next) {
   const stack = err?.stack
   const statusCode = err?.statusCode ? err?.statusCode : 500
   const message = err?.message
 
   return res.status(statusCode).json({ stack, message })
+}
+
+// 404 Handler
+
+export function notFound(req, res, next) {
+  const err = new Error(`Route ${req.originalUrl} not found!`)
+  next(err)
 }
