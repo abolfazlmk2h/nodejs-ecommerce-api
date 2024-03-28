0a143c7f3c1b940af4b563b32f63366ea5a4f5d5
abolfazlmk2h <mirzaei4273@gmail.com>
2024-03-28 17:25:55 +0330

    feat: add filtering products feature

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -48,7 +48,41 @@ export async function createProductController(req, res, next) {
 // @access Public
 
 export async function getProductsController(req, res) {
-  const products = await Product.find()
+  let productQuery = Product.find()
+
+  if (req.query.name) {
+    productQuery = productQuery.find({
+      name: { $regex: req.query.name, $options: 'i' },
+    })
+  }
+  if (req.query.brand) {
+    productQuery = productQuery.find({
+      brand: { $regex: req.query.brand, $options: 'i' },
+    })
+  }
+  if (req.query.category) {
+    productQuery = productQuery.find({
+      category: { $regex: req.query.category, $options: 'i' },
+    })
+  }
+  if (req.query.colors) {
+    productQuery = productQuery.find({
+      colors: { $regex: req.query.colors, $options: 'i' },
+    })
+  }
+  if (req.query.sizes) {
+    productQuery = productQuery.find({
+      sizes: { $regex: req.query.sizes, $options: 'i' },
+    })
+  }
+  if (req.query.price) {
+    const [minPrice, maxPrice] = req.query.price.split('-')
+    productQuery = productQuery.find({
+      price: { $gte: minPrice, $lte: maxPrice },
+    })
+  }
+
+  const products = await productQuery
   return res.json({
     status: 'success',
     products,
